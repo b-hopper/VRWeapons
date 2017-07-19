6b80a0e514b0eb6e3a9ba677b92c1c38804465d4
Xefier <umwelwoe@myumanitoba.ca>
2017-07-18 20:23:02 -0500

    Fixed VRW_ControllerActions_VRTK.TriggerAxisChanged Null Reference - Removed debug log from release builds

--- Scripts/VRTK/VRW_ControllerActions_VRTK.cs
+++ Scripts/VRTK/VRW_ControllerActions_VRTK.cs
@@ -25,10 +25,13 @@ public class VRW_ControllerActions_VRTK : MonoBehaviour
 
     private void TriggerAxisChanged(object sender, VRTK.ControllerInteractionEventArgs e)
     {
-        Debug.Log("Holding device: " + CurrentHeldWeapon.holdingDevice);
         if (CurrentHeldWeapon != null && e.controllerReference.scriptAlias == CurrentHeldWeapon.holdingDevice)
         {
             CurrentHeldWeapon.SetTriggerAngle(e.buttonPressure);
+            if (Debug.isDebugBuild)
+            {
+                Debug.Log("Holding device: " + CurrentHeldWeapon.holdingDevice);
+            }
         }
     }
 }
