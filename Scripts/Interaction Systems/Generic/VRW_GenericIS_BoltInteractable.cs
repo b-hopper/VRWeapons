0bb47b6cb9005d446043383e93514723c45a2737
Slayd7 <brad.chopper@gmail.com>
2017-07-25 20:06:24 -0700

    Generic IS bolt interaction complete
    Bolt interaction for generic IS is complete. Need to add it to the custom editor.
    
    Also, moved around some folders to clean it up some. Interaction systems now have their own folder, with individual sub-folders under them.

---
+++ Scripts/Interaction Systems/Generic/VRW_GenericIS_BoltInteractable.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VRWeapons.InteractionSystems.Generic
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class VRW_GenericIS_BoltInteractable : MonoBehaviour
+    {
+        IBoltActions bolt;
+
+        Collider col;
+
+        SteamVR_Controller.Device device;
+        SteamVR_TrackedObject trackedObj;
+
+        VRW_GenericIS_InteractableWeapon weaponInteractable;
+
+        Weapon thisWeap;
+
+        bool thisObjectIsGrabbed, hasMoved;
+
+        float lerpValue, dropTime;
+
+        [SerializeField]
+        public Vector3 boltOpenPosition, boltClosedPosition;
+
+        [SerializeField]
+        bool mustHoldGrabButton;
+
+        private void Start()
+        {
+            thisWeap = GetComponentInParent<Weapon>();
+            col = GetComponent<Collider>();
+            Physics.IgnoreCollision(col, thisWeap.weaponBodyCollider);
+            if (thisWeap.secondHandGripCollider != null)
+            {
+                Physics.IgnoreCollision(col, thisWeap.secondHandGripCollider);
+            }
+            bolt = transform.parent.GetComponentInChildren<IBoltActions>();
+            GetComponent<Rigidbody>().isKinematic = true;
+            weaponInteractable = GetComponentInParent<VRW_GenericIS_InteractableWeapon>();
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            if (!thisObjectIsGrabbed)
+            {
+                if (device == null || trackedObj == null)
+                {
+                    if ((trackedObj = other.GetComponentInParent<SteamVR_TrackedObject>()) != null)
+                    {
+                        device = SteamVR_Controller.Input((int)trackedObj.index);
+                    }
+                    if (trackedObj == weaponInteractable.trackedObj)
+                    {
+                        device = null;
+                        trackedObj = null;
+                    }
+                }
+                if (device != null && device.GetPressDown(weaponInteractable.grabButton) && Time.time - dropTime > 0.2f)
+                {
+                    thisObjectIsGrabbed = true;
+                    dropTime = Time.time;
+                }
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (!thisObjectIsGrabbed)
+            {
+                device = null;
+                trackedObj = null;
+            }
+        }
+
+        private void Update()
+        {
+            float oldLerpValue = lerpValue;
+
+            if (thisObjectIsGrabbed)
+            {
+                {
+                    bolt.IsCurrentlyBeingManipulated(true);
+                    hasMoved = true;
+
+                    transform.position = trackedObj.transform.position;
+
+                    ClampControllerToTrack();                                                                                       // Clamps to make sure it stays on the tracks it has been assigned in inspector
+
+                    lerpValue = VRWControl.V3InverseLerp(boltClosedPosition, boltOpenPosition, transform.localPosition);            // Final lerp value of bolt, which is then passed...
+
+                    bolt.boltLerpVal = lerpValue;                                                                                   // ... to the bolt itself.
+                }
+                if (device == null ||
+                    (device.GetPressUp(weaponInteractable.grabButton) && mustHoldGrabButton) ||
+                    (device.GetPressDown(weaponInteractable.grabButton) && Time.time - dropTime > 0.2f))
+                {
+                    thisObjectIsGrabbed = false;
+                }
+            }
+            else if (hasMoved)
+            {
+                bolt.IsCurrentlyBeingManipulated(false);
+                hasMoved = false;
+                ClampControllerToTrack();
+            }
+            else
+            {
+                lerpValue = bolt.boltLerpVal;
+            }
+            if (lerpValue != oldLerpValue)                                                                                      // Moves the manipulator to be in the correct position (with the bolt object). Only 
+            {                                                                                                                   // moves it if the lerp value has changed - doesn't need adjustment otherwise.
+                transform.localPosition = Vector3.Lerp(boltClosedPosition, boltOpenPosition, lerpValue);
+            }
+        }
+
+        void ClampControllerToTrack()
+        {
+            transform.localPosition = VRWControl.V3Clamp(transform.localPosition, boltOpenPosition, boltClosedPosition);
+        }
+
+    }
+}
---
+++ Scripts/Interaction Systems/Generic/VRW_GenericIS_InteractableWeapon.cs
@@ -0,0 +1,229 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VRWeapons.InteractionSystems.Generic
+{
+    [RequireComponent(typeof(Rigidbody))]
+
+    public class VRW_GenericIS_InteractableWeapon : MonoBehaviour
+    {
+        Weapon thisWeap;
+
+        SteamVR_Controller.Device device, secondHandDevice;
+        [HideInInspector]
+        public SteamVR_TrackedObject trackedObj, secondHandTrackedObj;
+
+        [SerializeField] public Valve.VR.EVRButtonId grabButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+        [SerializeField] Valve.VR.EVRButtonId fireButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+        [SerializeField] Valve.VR.EVRButtonId dropMagButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad;
+
+        [Tooltip("If checked, second hand grip button must be held down to remain gripping."), SerializeField]
+        public bool holdButtonTo2HandGrip;
+
+        Rigidbody thisRB;
+
+        bool isHeld, secondHandGripped, wasPreviouslyKinematic, isColliding;
+
+        Transform previousParent;
+
+        float dropTime;
+
+        private void Start()
+        {
+            thisWeap = GetComponent<Weapon>();
+            thisWeap.shotHaptics += new VRWControl.TriggerHaptics(ShotHaptics);
+            thisRB = GetComponent<Rigidbody>();
+            DisableWeaponColliders(isHeld);
+        }
+
+        void ShotHaptics()
+        {
+            Debug.Log("Haptics haptics");
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            if (!isHeld)
+            {
+                if (device == null || trackedObj == null)
+                {
+                    if ((trackedObj = other.GetComponentInParent<SteamVR_TrackedObject>()) != null)
+                    {
+                        device = SteamVR_Controller.Input((int)trackedObj.index);
+                    }
+                    isColliding = true;
+                }
+                if (device != null && device.GetPressDown(grabButton) && Time.time - dropTime > 0.2f)
+                {
+                    wasPreviouslyKinematic = thisRB.isKinematic;
+                    thisRB.isKinematic = true;
+                    previousParent = transform.parent;
+                    transform.parent = trackedObj.transform;
+                    transform.localPosition = thisWeap.grabPoint.localPosition;
+                    transform.localEulerAngles = thisWeap.grabPoint.localEulerAngles;
+                    isHeld = true;
+                    dropTime = Time.time;
+
+                    DisableWeaponColliders(isHeld);
+                }
+            }
+            else
+            {
+                if (secondHandDevice == null || secondHandTrackedObj == null)
+                {
+                    if ((secondHandTrackedObj = other.GetComponentInParent<SteamVR_TrackedObject>()) != null)
+                    {
+                        secondHandDevice = SteamVR_Controller.Input((int)secondHandTrackedObj.index);
+                    }
+                    isColliding = true;
+                }
+                if (secondHandDevice != null && secondHandDevice.GetPressDown(grabButton) && Time.time - dropTime > 0.2f && !secondHandGripped)
+                {
+                    secondHandGripped = true;
+                    dropTime = Time.time;
+                }
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            isColliding = false;
+        }
+
+        private void Update()
+        {
+            if (isHeld)
+            {
+                if (device.GetPressDown(fireButton))
+                {
+                    thisWeap.StartFiring(trackedObj.gameObject);
+                }
+                else if (device.GetPressUp(fireButton))
+                {
+                    thisWeap.StopFiring(trackedObj.gameObject);
+                }
+                if (device.GetTouch(fireButton))
+                {
+                    thisWeap.SetTriggerAngle(device.GetAxis(fireButton).x);
+                }
+                if (device.GetPressDown(dropMagButton))
+                {
+                    thisWeap.DropMagazine();
+                }
+                if (device.GetPressDown(grabButton) && Time.time - dropTime > 0.2f)
+                {
+                    thisRB.isKinematic = wasPreviouslyKinematic;
+                    transform.parent = previousParent;
+
+                    tossObject(thisRB);
+
+                    isHeld = false;
+
+                    DisableWeaponColliders(isHeld);
+
+                    dropTime = Time.time;
+                    device = null;
+                    trackedObj = null;
+                }
+                if (secondHandGripped)
+                {
+                    ZLockedAim();
+                    if ((secondHandDevice.GetPressDown(grabButton) || (secondHandDevice.GetPressUp(grabButton) && holdButtonTo2HandGrip)) && Time.time - dropTime > 0.2f)
+                    {
+                        secondHandGripped = false;
+                        Realign();
+                    }
+                }
+            }
+            else if (!isColliding && !isHeld)
+            {
+                device = null;
+                trackedObj = null;
+                secondHandDevice = null;
+                secondHandTrackedObj = null;
+            }
+        }
+
+        void tossObject(Rigidbody rigidbody)
+        {
+            Transform origin = trackedObj.origin ? trackedObj.origin : trackedObj.transform.parent;
+
+            if (origin != null)
+            {
+                rigidbody.velocity = origin.TransformVector(device.velocity);
+                rigidbody.angularVelocity = origin.TransformVector(device.angularVelocity);
+            }
+            else
+            {
+                rigidbody.velocity = device.velocity;
+                rigidbody.angularVelocity = device.angularVelocity;
+            }
+        }
+
+        void DisableWeaponColliders(bool isGripped)
+        {
+            thisWeap.weaponBodyCollider.enabled = !isGripped;
+            if (thisWeap.secondHandGripCollider != null)
+            {
+                thisWeap.secondHandGripCollider.enabled = isGripped;
+            }
+        }
+
+        protected virtual void ZLockedAim()                     // BORROWED FROM VRTK'S VRTK_ControlDirectionGrabAction SCRIPT, THANKS STONE FOX//
+        {
+            Vector3 forward = (secondHandTrackedObj.transform.position - trackedObj.transform.position).normalized;
+
+            // calculate rightLocked rotation
+            Quaternion rightLocked = Quaternion.LookRotation(forward, Vector3.Cross(-trackedObj.transform.right, forward).normalized);
+
+            // delta from current rotation to the rightLocked rotation
+            Quaternion rightLockedDelta = Quaternion.Inverse(transform.rotation) * rightLocked;
+
+            float rightLockedAngle;
+            Vector3 rightLockedAxis;
+
+            // forward direction and roll
+            rightLockedDelta.ToAngleAxis(out rightLockedAngle, out rightLockedAxis);
+
+            if (rightLockedAngle > 180f)
+            {
+                // remap ranges from 0-360 to -180 to 180
+                rightLockedAngle -= 360f;
+            }
+
+            // make any negative values into positive values;
+            rightLockedAngle = Mathf.Abs(rightLockedAngle);
+
+            // calculate upLocked rotation
+            Quaternion upLocked = Quaternion.LookRotation(forward, trackedObj.transform.forward);
+
+            // delta from current rotation to the upLocked rotation
+            Quaternion upLockedDelta = Quaternion.Inverse(transform.rotation) * upLocked;
+
+            float upLockedAngle;
+            Vector3 upLockedAxis;
+
+            // forward direction and roll
+            upLockedDelta.ToAngleAxis(out upLockedAngle, out upLockedAxis);
+
+            // remap ranges from 0-360 to -180 to 180
+            if (upLockedAngle > 180f)
+            {
+                upLockedAngle -= 360f;
+            }
+
+            // make any negative values into positive values;
+            upLockedAngle = Mathf.Abs(upLockedAngle);
+
+            // assign the one that involves less change to roll
+            transform.rotation = (upLockedAngle < rightLockedAngle ? upLocked : rightLocked);
+        }
+
+        void Realign()
+        {
+            transform.localEulerAngles = thisWeap.grabPoint.localEulerAngles;
+            transform.localPosition = thisWeap.grabPoint.localPosition;
+        }
+    }
+}
