5232ae0d1a03f61b9c42f55147d687dd9ff961a5
Xefier <umwelwoe@myumanitoba.ca>
2017-09-02 11:03:04 -0500

    Implemented Ejector.forceMode/enableCollider

--- Scripts/Ejector.cs
+++ Scripts/Ejector.cs
@@ -15,13 +15,28 @@ namespace VRWeapons
         [SerializeField]
         float ejectorRotationalRandomness = 45;
 
+        [SerializeField]
+        private ForceMode forceMode = ForceMode.Impulse;
+
+        [SerializeField]
+        private bool enableCollider = false;
+
         public void Eject(Transform t, Rigidbody rb)
         {
             rb.isKinematic = false;
             Vector3 forward = (transform.forward + (Random.insideUnitSphere * ejectorRandomness)).normalized;
             rb.AddForce(forward * ejectForce, ForceMode.Impulse);
-            rb.AddTorque(Random.insideUnitSphere * ejectorRotationalRandomness, ForceMode.Impulse);
+            rb.AddTorque(Random.insideUnitSphere * ejectorRotationalRandomness, forceMode);
             t.parent = null;
+
+            if(enableCollider)
+            {
+                var collider = rb.GetComponent<Collider>();
+                if(collider != null)
+                {
+                    collider.enabled = true;
+                }
+            }
         }
     }
 }
